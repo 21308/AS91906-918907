bfce0273a3c25551d76e5602a4e2c649bd91e92e
21308 <133619381+21308@users.noreply.github.com>
2025-03-21 13:54:53 +1300

    Friday 21 March 2025

--- C_03_Help_GUI.py
+++ C_03_Help_GUI.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from functools import partial # To prevent unwanted windows
 
 class Converter:
     """"
@@ -23,15 +23,74 @@ class Converter:
         self.to_help_button.grid(row=1, padx=5, pady=5)
 
     def to_help(self):
-        print("You pushed the help button")
+        DisplayHelp(self)
 
 
 
+class DisplayHelp:
+
+    def __init__(self, partner):
+
+        # setup dialogue box and background colour
+        background = "#ffe6cc"
+        self.help_box = Toplevel()
+
+        # disable help button
+        partner.to_help_button.config(state=DISABLED)
+
+        self.help_frame = Frame(self.help_box, width=300,
+                            height=200)
+        self.help_frame.grid()
+
+        self.help_heading_label = Label(self.help_frame,
+                                    text="Help / Info",
+                                    font=("Arial", "14", "bold"))
+        self.help_heading_label.grid(row=0)
+
+        help_text = "To use the program, simply enter the temperature " \
+                    "you wish to convert and then choose to convert " \
+                    "to either degrees Celsius (centigrade) or " \
+                    "Fahrenheit... \n\n" \
+                    " Note that -273 degrees C " \
+                    "(-459 F) is absolute zero (the coldest possible " \
+                    "temperature). If you try to convert a " \
+                    "temperature that is less than -273 degrees C, " \
+                    "you will get an error message. \n\n " \
+                    "To see your " \
+                    "calculation history and export it to a text " \
+                    "file, please click the 'History / Export' button."     
 
 
-   # main routine
-    if __name__ == "__main__":
-     root = Tk()
-     root.title("Temperature Converter")
-     Converter()
-     root.mainloop()
+        self.help_text_label = Label(self.help_frame,
+                                 text=help_text, wraplength=350,
+                                 justify="left")
+        self.help_text_label.grid(row=1, padx=10)
+
+        self.dismiss_button = Button(self.help_frame,
+                                 font=("Arial", "12", "bold"), 
+                                 text="Dismiss", bg="#CC6600",
+                                 fg="#FFFFFF", 
+                                 command=partial(self.close_help, partner))
+        self.dismiss_button.grid(row=2, padx=10, pady=10)
+
+
+        # List and loop to set backgroudn colour on
+        # everything except the buttons.
+        recolour_list = [self.help_frame, self.help_heading_label,
+                         self.help_text_label]
+        
+        for item in recolour_list:
+            item.config(bg=background)
+
+
+    def close_help(self, partner):
+        partner.to_help_button.config(stae=NORMAL)
+        self.help_box.destroy()
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
